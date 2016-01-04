664b01d72b2ee7daf5408bf2fb59fcd393abc3e6
Anderson Grudtner Martins <anderson@ostraining.com>
2016-01-04 18:26:30 -0200

    Fix the copyright comments

--- src/include.php
+++ src/include.php
@@ -2,7 +2,7 @@
 /**
  * @package   OSEmbed
  * @contact   www.ostraining.com, support@ostraining.com
- * @copyright 2013-2014 Open Source Training, LLC. All rights reserved
+ * @copyright 2016 Open Source Training, LLC. All rights reserved
  * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
 
