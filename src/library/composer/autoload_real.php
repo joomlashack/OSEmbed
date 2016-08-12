62f0d8175eab76ca77a17a0408458bb700b29627
Anderson Grudtner Martins <anderson@ostraining.com>
2016-08-12 15:28:44 -0300

    Updates Embera to 1.9.4 fixing issue with hyphen on Instagram URL

--- src/library/composer/autoload_real.php
+++ src/library/composer/autoload_real.php
@@ -23,28 +23,30 @@ class ComposerAutoloaderInit15b942fc6af6974ec5ec3a1e7a92e45dF
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit15b942fc6af6974ec5ec3a1e7a92e45dF', 'loadClassLoader'));
 
-        $map = require __DIR__ . '/autoload_namespaces.php';
-        foreach ($map as $namespace => $path) {
-            $loader->set($namespace, $path);
-        }
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        if ($useStaticLoader) {
+            require_once __DIR__ . '/autoload_static.php';
 
-        $map = require __DIR__ . '/autoload_psr4.php';
-        foreach ($map as $namespace => $path) {
-            $loader->setPsr4($namespace, $path);
-        }
+            call_user_func(\Composer\Autoload\ComposerStaticInit15b942fc6af6974ec5ec3a1e7a92e45dF::getInitializer($loader));
+        } else {
+            $map = require __DIR__ . '/autoload_namespaces.php';
+            foreach ($map as $namespace => $path) {
+                $loader->set($namespace, $path);
+            }
 
-        $classMap = require __DIR__ . '/autoload_classmap.php';
-        if ($classMap) {
-            $loader->addClassMap($classMap);
+            $map = require __DIR__ . '/autoload_psr4.php';
+            foreach ($map as $namespace => $path) {
+                $loader->setPsr4($namespace, $path);
+            }
+
+            $classMap = require __DIR__ . '/autoload_classmap.php';
+            if ($classMap) {
+                $loader->addClassMap($classMap);
+            }
         }
 
         $loader->register(true);
 
         return $loader;
     }
-}
-
-function composerRequire15b942fc6af6974ec5ec3a1e7a92e45dF($file)
-{
-    require $file;
 }
