5a3b88972f095f5b45ac62e3c7b06d1aaa8db306
Valentín <htmgarcia@gmail.com>
2016-12-17 13:30:32 -0600

    Test code for Facebook responsiveness (commented)

--- src/media/js/osembed.js
+++ src/media/js/osembed.js
@@ -8,7 +8,7 @@
 (function($)
 {
     $(window).resize(function() {
-        $('.ose-flickr').each(function() {
+        $('.ose-flickr, .ose-facebook').each(function() {
 
             // Calculate old and new width/height values
             var $oldHeight  =  $(this).find('iframe').attr('height'); // Get iframe's height
@@ -21,6 +21,18 @@
                 "height" : $newHeight + "px",
                 "width" : $newWidth + "px"
             });
+
+            // Only for Facebook
+            /*if( $(this).is('.ose-facebook') ) {
+             console.log('Facebook');
+
+             $(this).find('.fb_iframe_widget, .fb_iframe_widget > span').css({
+             "height" : $newHeight + "px",
+             "width" : $newWidth + "px"
+             });
+
+             console.log( $(this).find('iframe').attr('name') );
+             }*/
         });
     });
 })(jQuery);
