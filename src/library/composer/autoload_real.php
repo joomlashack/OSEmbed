e88c42cd997ce4ef517320e899105eacb442d0ba
Bill Tomczak <bill@ostraining.com>
2017-03-21 14:20:23 -0700

    Run composer update

--- src/library/composer/autoload_real.php
+++ src/library/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit15b942fc6af6974ec5ec3a1e7a92e45dF
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit15b942fc6af6974ec5ec3a1e7a92e45dF', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
